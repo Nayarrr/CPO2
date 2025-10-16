fc0c8ecece4d93bc7c9e44aff8b2161ef3bb7f18
Nayarr <rayan.tournay@etu.umontpellier.fr>
2025-10-16 11:23:36 +0200

    Fini le truc tah la pom

---
+++ src/Rapp/src/main/java/Tournay/Rayan/App.java
@@ -0,0 +1,13 @@
+package src.Rapp.src.main.java.Tournay.Rayan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
