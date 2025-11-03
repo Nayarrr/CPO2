0f9f19f51d7528a9a2db0a9af9f9cc5cfc9d5f35
Nayarr <rayan.tournay@etu.umontpellier.fr>
2025-11-03 13:00:12 +0100

    A faire

---
+++ src/Jpa-App/src/main/java/Tournay/Rayan/App.java
@@ -0,0 +1,13 @@
+package Tournay.Rayan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
